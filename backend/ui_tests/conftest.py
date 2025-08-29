9933a2970d5025aab6f67418a33c297ee0925446
Svetlana Zubrilina <lana.zubrilina@gmail.com>
2025-08-29 17:34:53 -0500

    Base url added

--- backend/ui_tests/conftest.py
+++ backend/ui_tests/conftest.py
@@ -2,9 +2,17 @@ import pytest
 from faker import Faker
 from playwright.sync_api import Page
 from components import signup, login,delete_user, items
-
+BASE_URL = "http://localhost:5173"
 fake = Faker()
  
+@pytest.fixture
+def page_baseurl(browser: Browser):
+    context = browser.new_context(base_url=BASE_URL)
+    page = context.new_page()
+    yield page
+    page.close()
+    context.close()
+    
 @pytest.fixture
 def sugnup_user(page: Page):
     fullname=fake.name()
