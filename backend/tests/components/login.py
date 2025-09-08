e8466f9f95abfcfffb46dafb8f6d49685fcbf7f4
John Makarov <johnmakarovqa@gmail.com>
2025-09-08 18:48:35 +0300

    add some allure steps

--- backend/tests/components/login.py
+++ backend/tests/components/login.py
@@ -1,13 +1,16 @@
 from playwright.sync_api import Page, expect
+from allure import step
 
 
 class LoginForm:
     def __init__(self, page: Page):
         self.page = page
 
+    @step("Open login form")
     def open(self):
         self.page.goto("/login")
 
+    @step("Login as user")
     def as_user(self, email: str, password: str):
         self.open()
         self.page.get_by_placeholder("Email").fill(email)
